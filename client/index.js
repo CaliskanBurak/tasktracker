1809b1e4596173a38efb248cc9803376686bad2c
Burak Caliskan <caliskan.burakali@gmail.ocm>
2020-08-11 20:03:58 -0400

    First version

---
+++ client/index.js
@@ -0,0 +1,16 @@
+ import React, { Component } from 'react';
+ import { render } from 'react-dom'; 
+
+
+ class App extends Component { 
+    render() {
+        return(
+            <div>
+                hellosss world! 
+            </div>
+        )
+    }
+ }
+
+
+ render(<App />, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry : './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+          {
+            test: /\.jsx?/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            }
+          },
+          {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+          }
+        ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'client/index.html'
+      })
+    ],
+}
